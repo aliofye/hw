69508c468d661558ff88612ffa6251571b065330
noneisnull <noneisnull@gmail.com>
2017-04-28 18:23:31 -0400

    updated test.cpp with more tests
    Signed-off-by: noneisnull <noneisnull@gmail.com>

--- cse250/a6/test.cpp
+++ cse250/a6/test.cpp
@@ -3,7 +3,7 @@
 
 int getHammingWeight(int num){
 	if(num < 0){
-		return 0; //doesn't accept neg numbers
+		return 0; //doesn't accept negative numbers
 	}
 
 	int w = 0;
@@ -17,10 +17,6 @@ int getHammingWeight(int num){
 
 int main()
 {
-	/*bool small = false;
-	bool large = false;
-	int a = 2; // 0010
-	int b = 15;// 1111
 
 	// 128 64 32 16 8 4 2 1
 	//			    1 0 1 0
@@ -29,39 +25,30 @@ int main()
 	//            1 0 1 0 0
 
 
-	std::cout << getHammingWeight(b) << std::endl;*/
-	int a = 1;
-	int b = 10;
+	//std::cout << getHammingWeight(b) << std::endl;
 	JobQueue jq;
-	Job newJob = Job(a, b);
-	jq.push(newJob);
-	a = 2; b = 1;
-	newJob = Job(a, b);
-	jq.push(newJob);
-	a = 5; b = 3;
-	newJob = Job(a, b);
-	jq.push(newJob);
-	a = 4; b = 3;
-	newJob = Job(a, b);
-	jq.push(newJob);
+	
+	for(int i = 0; i < 100; i++){
+		int a = std::rand()%(250-0 + 1) + 0;
+
+		int b = std::rand()%(250-0 + 1) + 0;
+		
+		Job newJob = Job(a, b);
+		jq.push(newJob);
+	}
+	
 
 	
 	try{
 
-		std::cout << jq.top()._pid << ", " << jq.top()._priority << std::endl;
+		for(int i = 0; i < 100; i++){
 
-		jq.pop();
+			std::cout << jq.top()._pid << ", " << jq.top()._priority << std::endl;
+			jq.pop();
+			
+		}
 
-		std::cout << jq.top()._pid << ", " << jq.top()._priority << std::endl;
-
-		jq.pop();
-
-		std::cout << jq.top()._pid << ", " << jq.top()._priority << std::endl;
-
-		jq.pop();
-
-		std::cout << jq.top()._pid << ", " << jq.top()._priority << std::endl;
-
+		
 
 	} catch(std::runtime_error e){
 		std::cout << e.what() << std::endl;
