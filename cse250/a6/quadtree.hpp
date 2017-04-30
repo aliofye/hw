1b7751147dea5bd2edc40c1deb135f30f2701f80
noneisnull <noneisnull@gmail.com>
2017-04-30 01:35:49 -0400

    fixed a stupid mistake with calling the constructor recursively, thinks went as well as one might think
    Signed-off-by: noneisnull <noneisnull@gmail.com>

--- cse250/a6/quadtree.hpp
+++ cse250/a6/quadtree.hpp
@@ -1,4 +1,5 @@
 #include <stack>
+#include <iostream>
 
 class QuadTreeNode {
 public:
@@ -10,31 +11,36 @@ class TreeIterator {
 private:
 	
 public:
-	std::stack<QuadTreeNode*> nodestk; 
-	
-	TreeIterator(QuadTreeNode* root) {
-	
+	std::stack<QuadTreeNode*> nodestk;
+
+	void iterate(QuadTreeNode* root){
+
 		if(root->child[0] != nullptr){
-			TreeIterator(root->child[0]);
+			iterate(root->child[0]);
 		}
 
 		if(root->child[1] != nullptr){
-			TreeIterator(root->child[1]);
+			iterate(root->child[1]);
 		}
 
 		if(root->child[2] != nullptr){
-			TreeIterator(root->child[2]);
+			iterate(root->child[2]);
 		}
 
 		if(root->child[3] != nullptr){
-			TreeIterator(root->child[3]);
+			iterate(root->child[3]);
 		}
 
 		nodestk.push(root);
-
+	} 
+	
+	TreeIterator(QuadTreeNode* root) {
+	
+		iterate(root);
 	}
 
 	TreeIterator& operator++() {
+		QuadTreeNode* node = nodestk.top();
 		nodestk.pop();
 	}
 		
