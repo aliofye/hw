35f3d78cae3c4cb1b566dcac504a907e2321364b
noneisnull <noneisnull@gmail.com>
2017-10-01 19:59:07 -0400

    getting somewhere
    Signed-off-by: noneisnull <noneisnull@gmail.com>

--- cse321/hw1/p2.c
+++ cse321/hw1/p2.c
@@ -2,70 +2,108 @@
 #include <ctype.h>
 #include <math.h>
 
+int duplicates[100][3];
+int pos = 0;
+char last[3];
+
+void printArray(int arr[], int arr_size, int score){
+    //declare constants
+    const unsigned short CONVERSION = 7;
+    const unsigned short TRIE = 3;
+    const unsigned short KICK = 2;
+
+    int c=0;
+    int t=0;
+    int k=0;
+
+
+    int i;
+    for (i = 0; i < arr_size; i++){
+        switch(arr[i]){
+            case 2:
+                k++;
+                break;
+            case 3:
+                t++;
+                break;
+            case 7:
+                c++;
+                break;
+        }
+    }
+
+    int total = (c*CONVERSION) + (t*TRIE) + (k*KICK);
+    char current[] = {(char) c, (char) t, (char) k};
+    
+    //check equality
+    int eqCount = 0;
+    for(i=0; i<3; ++i){
+        if(last[i] == current[i]){
+            eqCount++;
+        }
+    }
+
+    if(total == score && eqCount!=3){
+        printf("%d", c);
+        printf("%2d", t);
+        printf("%2d\n", k);
+
+        last[0] = (char) c;
+        last[1] = (char) t;
+        last[2] = (char) k;
+
+        /*
+        duplicates[pos][0] = c;
+        duplicates[pos][1] = t;
+        duplicates[pos][2] = k;
+
+        pos++;*/
+    }  
+}
+
+void printCompositions(int n, int i, int score){
+        /* array must be static as we want to keep track
+        of values stored in arr[] using current calls of
+        printCompositions() in function call stack*/
+        static int arr[100];
+         
+        if (n == 0)
+        {
+            printArray(arr, i, score);
+        }
+        else if(n > 0)
+        {
+            int k; 
+            for (k = 2; k <= 7; k++)
+            {
+                arr[i]= k;
+                printCompositions(n-k, i+1, score);
+            }
+        }
+    }
+ 
+
 int main(){
     
    	//get score
     unsigned int score;
     scanf("%d", &score);
 
-    //declare constants
-    const unsigned short CONVERSION = 7;
-    const unsigned short TRIE = 3;
-    const unsigned short KICK = 2;
 
-    int pset[7][3] = {{0,0,2}, {0,3,0}, {0,3,2}, {7,0,0}, {7,0,2}, {7,3,0}, {7,3,2}};
-    
-    int i=0;
-    
-    for(i=0; i<7; ++i){
-        int c = 0;
-        int t = 0;
-        int k = 0;
+    int points[3] = {7,3,2};
 
-        int remainder = score;
-        int j = 0;
+    printCompositions(score, 0, score);
 
-        for(j=0; j<3; j++){
-
-            int x = pset[i][j];
-            int tmp = x;
-            if(x != 0){
-                while(tmp <= remainder){//12<10
-                    
-                    tmp+=x;//tmp=12
-
-                    if(j==0){
-                        c++;
-                    } else if(j==1){
-                        t++;//t=3
-                    } else{
-                        k++;
-                    }
-                }
-
-                if(tmp < remainder){
-                    remainder-= tmp; //remainder = 4
-
-                    if(remainder < pset[i][j+1]){//no
-                        remainder+=x;
-                    }
-
-                } else{
-                    tmp-=x;//tmp=9
-                    remainder-=tmp; //remainder = 1
-
-                    if(remainder < pset[i][j+1]){//yes
-                        remainder+=x; //remainder = 4
-                    }
-                }
-            }
+    /*int i=0;
+    for(i=0; i<100; ++i){
+        
+        int j=0;
+        for (j=0; j < 3; ++j){
+           printf("%d", duplicates[i][j]);
         }
 
-        printf("%d", c);
-        printf("%2d", t);
-        printf("%2d\n", k);
-    }
-
+        printf("\n");
+    }*/
 
     return 0;
 }
