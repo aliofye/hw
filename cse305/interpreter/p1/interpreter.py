4f6251c98c5b73f54ed4b675dd699a5d887c192a
Ali Morshid <noneisnull@gmail.com>
2018-04-01 23:28:51 -0400

    v0.8

--- cse305/interpreter/p1/interpreter.py
+++ cse305/interpreter/p1/interpreter.py
@@ -12,14 +12,26 @@ def interpreter(input, output):
 			token = push_helper(line)
 			stack.append(token)
 		if "pop" in line:
-			stack = pop_handler(stack)
+			stack = pop_helper(stack)
 		if "add" in line:
-			stack = add_handler(stack)
+			stack = math_helper(stack, "add")
 		if "sub" in line:
-			stack = sub_handler(stack)
-	# print stack
-	for token in stack:
-		output_file.write(str(token) + "\n");
+			stack = math_helper(stack, "sub")
+		if "mul" in line:
+			stack = math_helper(stack, "mul")
+		if "div" in line:
+			stack = math_helper(stack, "div")
+		if "rem" in line:
+			stack = math_helper(stack, "rem")
+		if "neg" in line:
+			stack = neg_helper(stack)
+		if "swap" in line:
+			stack = swap_helper(stack)
+		if "quit" in line:
+			# print stack
+			while len(stack) != 0:
+				output_file.write(str(stack.pop()) + "\n");
+	
 
 	#close streams
 	input_file.close()
@@ -27,35 +39,47 @@ def interpreter(input, output):
 
 def push_helper(line):
 	# sanitize input
-	line.replace("push", "")
-	# try to match integers
-	token = re.findall("\-?\d+", line)[0]
+	line = line.replace("push", "")
+	line = line.strip()
+	# try to match boolean or error literals
+	token = re.match(r"(:true:|:false:|:error:)", line)
 	if token is not None:
+		token = token.group()
+		print("literals", token)
 		return token
 	# try to match strings
-	token = re.findall(r'"(.*?)"', line)[0]
+	token = re.match(r'"(.*?)"', line)
 	if token is not None:
+		token = token.group()
+		print("strings", token)
 		return token
 	# try to match names
-	token = re.findall("[a-zA-Z]+\d*", line)[0]
+	token = re.match(r"[a-zA-Z]+\d*", line)
 	if token is not None:
+		token = token.group()
+		print("names", token)
 		return token
-	# try to match boolean or error literals
-	token = re.findall("(:true:|:false:|:error:)", line)[0]
+	# try to match integers
+	token = re.match("\-?\d+", line)
 	if token is not None:
+		token = token.group()
+		if token == "-0":
+			token = "0"
+		print("numbers", token)
 		return token
+	
 	# not able to match anything
 	token = ":error:"
 	return token
 
-def pop_handler(stack):
+def pop_helper(stack):
 	if not stack:
 		stack.append(":error:")
 	else:
 		stack.pop()
 	return stack
 
-def add_handler(stack):
+def math_helper(stack, op):
 	if len(stack) <= 1:
 		stack.append(":error:")
 	else:
@@ -64,16 +88,56 @@ def add_handler(stack):
 		try:
 			lhs = int(lhs)
 			rhs = int(rhs)
-			total = lhs + rhs
+			# switch operations
+			if "add" in op:
+				total = (lhs + rhs)
+			elif "sub" in op:
+				total = (rhs - lhs)
+			elif "mul" in op:
+				total = (lhs * rhs)
+			elif "div" in op:
+				# divison by zero
+				if rhs == 0:
+					raise ValueError("division by zero")
+				total = (lhs / rhs)
+			elif "rem" in op:
+				# divison by zero
+				if rhs == 0:
+					raise ValueError("division by zero")
+				total = (lhs % rhs)
+			# append total
 			stack.append(total)
 		except ValueError:
 			lhs = str(lhs)
 			rhs = str(rhs)
 			stack.append(rhs)
-			stack.append(lhs)	
+			stack.append(lhs)
+			stack.append(":error:")	
 	return stack
 
-def sub_handler(stack):
+def neg_helper(stack):
+	if not stack:
+		stack.append(":error:")
+	else:
+		token = stack.pop()
+		try:
+			num = int(token)
+			num = -num
+			stack.append(num)
+		except ValueError:
+			stack.append(token)
+			stack.append(":error:")
 	return stack
-	
-interpreter("./input/input2.txt", "output.txt")
+
+def swap_helper(stack):
+	if len(stack) <= 1:
+		stack.append(":error:")
+	else:
+		lhs = stack.pop()
+		rhs = stack.pop()
+		# swap
+		stack.append(lhs)
+		stack.append(rhs)
+	return stack
+
+interpreter("./input.txt", "output.txt")
